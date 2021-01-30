39ede5a7355d07d66af1e5180f97e471fe674780
Anthony DelSordo <anthonydelsordo@yahoo.com>
2021-01-30 11:33:38 -0500

    added player needs script

---
+++ Assets/Scripts/PlayerNeeds.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNeeds : MonoBehaviour
+{
+    [SerializeField] float hungerLevel;
+    [SerializeField] float thirstLevel;
+    [SerializeField] float hungerDropModifier = 1;
+    [SerializeField] float thirstDropModifier = 1;
+    const float maxHungerLevel = 100;
+    const float maxThirstLevel = 100;
+
+    // Start is called before the first frame update
+    void Start() {
+        hungerLevel = maxHungerLevel;
+        thirstLevel = maxThirstLevel;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        DepleteNeeds();
+    }
+
+    void DepleteNeeds() {
+        hungerLevel -= hungerDropModifier * Time.deltaTime;
+        thirstLevel -= thirstDropModifier * Time.deltaTime;
+
+        if (hungerLevel <= 0) {
+            Debug.Log("You are dying of hunger");
+        }
+
+        if (thirstLevel <= 0) {
+            Debug.Log("You are dying of thirst");
+        }
+    }
+}
