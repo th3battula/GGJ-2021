ff2fa98a65a8a9c7f806f2fe715f469df6930333
Anthony DelSordo <anthonydelsordo@yahoo.com>
2021-01-31 16:25:21 -0500

    added drone logic

---
+++ Assets/Scripts/Drone.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drone : MonoBehaviour {
+    [SerializeField] float movementSpeed;
+    Vector3 endPosition;
+    [SerializeField] float detectionChance;
+    // Start is called before the first frame update
+    void Start() {
+        endPosition = GameObject.Find("DroneEnd").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float step =  movementSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, endPosition, step);
+
+        GameObject firePit = GameObject.FindGameObjectWithTag("FirePit");
+        if (firePit) {
+            float randValue = Random.Range(0, 1f);
+            bool didFindFire = randValue <= detectionChance;
+            if (didFindFire) {
+                Debug.Log("YOU WIN!!!!!!111!!1!11");
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.name == "DroneEnd") {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/DroneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneController : MonoBehaviour {
+    [SerializeField] GameObject dronePrefab;
+    [SerializeField] float spawnTimer;
+    [SerializeField] float maxSpawnTimer;
+
+    // Start is called before the first frame update
+    void Start() {
+        spawnTimer = maxSpawnTimer;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0) {
+            Instantiate(dronePrefab, GameObject.Find("DroneStart").transform.position, Quaternion.identity);
+            spawnTimer = maxSpawnTimer;
+        }
+    }
+}
